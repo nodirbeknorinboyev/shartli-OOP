f2b70eae31909c07c4548277ce2fb69ac6a02887
nodir2208 <norinboyevnortoy544@gmail.com>
2024-02-19 12:03:18 +0500

    #shartli oop

---
+++ shartli_operatorlar/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const age = +prompt("How old are yu?", " ");
+// if(age > 25){
+//     console.log("Horror films");
+// }else if(age > 18 && age < 25){
+    
+// }else{
+//     console.log("Corton");
+// }
+
+// Ternade operatorlar
+ //age > 25 ? console.log("Horror films") :console.log("Corton");
+
+ const color = "crashet"
+
+ switch(color){
+    case "red":
+        console.log("Stop");
+        break;
+     case "green":
+        console.log("Goo");
+        break;
+    case "yellow":
+        console.log("Slowly");
+        break;
+        default:
+        console.log("Traffic jam");
+        break;    
+ }
